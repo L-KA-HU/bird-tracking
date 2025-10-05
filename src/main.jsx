a9957bd6f638529086f84587dcecc80b7a66f0e6
Lovas-Kiss Ádám <epipactispalustris@gmail.com>
2025-10-05 22:04:58 +0200

    Wrap App with SettingsProvider + favicon

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(<App />)
+// Most projects export a named SettingsProvider:
+import { SettingsProvider } from './Settings'
+// If you get a build error saying it's not exported,
+// use this instead:
+// import SettingsProvider from './Settings'
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SettingsProvider>
+      <App />
+    </SettingsProvider>
+  </React.StrictMode>
+)
