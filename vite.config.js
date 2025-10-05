657f61c11e724dc5ff8d9f882efa65a39c0d1f32
Lovas-Kiss Ádám <epipactispalustris@gmail.com>
2025-10-05 22:01:24 +0200

    Rebuild for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bird-tracking/',   // GitHub Pages repo subpath
-  build: { outDir: 'docs' }  // Pages serves from /docs
+  base: '/bird-tracking/',   // GitHub Pages subpath
+  build: { outDir: 'docs' }  // Pages serves /docs
 })
